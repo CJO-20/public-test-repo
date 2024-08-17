2e2bd332d5304ad10c185a03de86b31c8792fd56
CJO-20 <cjoneill@icloud.com>
2024-08-17 16:56:48 -0400

    Adding more complexity with cmake

---
+++ include/test.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+int test_func(int a, int b)
+{
+    return a + b;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 
+#include "test.hpp"
+
 int main (const int argc, const char * argv[])
 {
     std::cout << "Hello, world!" << std::endl;
+    std::cout << test_func(3, 4) << std::endl;
     return 0;
 }
