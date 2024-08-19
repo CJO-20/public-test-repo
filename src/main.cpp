9ae1b146e4aca2cd93513333fe6c2fa6ea2e2c1d
CJO-20 <cjoneill@icloud.com>
2024-08-19 00:43:53 -0400

    Adding test for dependency on conan + Poco

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,29 @@
 #include <iostream>
+#include <sstream>
+#include <Poco/JSON/Object.h>
+#include <Poco/JSON/Array.h>
 
-#include "test.hpp"
-
-int main (const int argc, const char * argv[])
+int main (int argc, const char * argv[])
 {
-    std::cout << "Hello, world!" << std::endl;
-    std::cout << test_func(3, 4) << std::endl;
+    Poco::JSON::Object base(Poco::JSON_PRESERVE_KEY_ORDER), secondary(Poco::JSON_PRESERVE_KEY_ORDER);
+    Poco::JSON::Array arr;
+    
+    arr.add("Hello");
+    arr.add("World");
+    
+    base.set("Key1", 42);
+    base.set("Key2", "null");
+    
+    secondary.set("s_Key1", "wow");
+    secondary.set("s_Key2", 3.14159265);
+    secondary.set("s_Key3", arr);
+    
+    base.set("Key3", secondary);
+    
+    base.stringify(std::cout, 4);
+    
+    std::cout << std::endl << std::endl;
+    
     return 0;
 }
+
